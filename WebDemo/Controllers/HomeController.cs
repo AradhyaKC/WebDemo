70752c5cef69d8bea62259e49b462aea0abd1c90
neivukc47@gmail.com <neivukc47@gmail.com>
2021-10-30 18:15:31 +0530

    Contact changed

--- WebDemo/Controllers/HomeController.cs
+++ WebDemo/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebDemo.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Changed";
 
             return View();
         }
