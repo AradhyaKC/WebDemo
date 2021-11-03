744003ceb6605cd94579a693d7445040996142ae
neivukc47@gmail.com <neivukc47@gmail.com>
2021-11-03 19:24:16 +0530

    Added webdemo companyModel and EmployeeModel

---
+++ WebDemo/Models/CompanyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace WebDemo.Models
+{
+    public class CompanyModel
+    {
+        [Display(Name ="Name of Company")]
+        [Required(ErrorMessage ="name of company should be entered")]
+        public string companyName { get; set; }
+
+        [Display(Name = "Motto")]
+        [Required(ErrorMessage = "Motto of company should be entered")]
+        public string motto;
+
+        [Display(Name = "Date when company was started")]
+        [Required(ErrorMessage = "Start date should be entered")]
+        public DateTime startDate;
+    }
+}
--- WebDemo/Models/EmployeeModel.cs
+++ WebDemo/Models/EmployeeModel.cs
@@ -8,43 +8,59 @@ namespace WebDemo.Models
 {
     public class EmployeeModel
     {
-        [Display(Name = "Emplayee ID" )]
-        [Required(ErrorMessage ="Employee ID is Required")]
-        [Range(100000,999999 ,ErrorMessage ="Must be a valid 6-digit number")]
-        public int EmployeeID { get; set; }
-        
+
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "First Name is Required")]
-        public string  FirstName { get; set; }
+        public string firstName { get; set; }
 
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Last Name is Required")]
-        public string LastName { get; set; }
+        public string lastName { get; set; }
 
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         [Required(ErrorMessage = "Email Address is Required")]
-        [StringLength(100,ErrorMessage ="Cannot Exceed 100 Characters")]
-        public string  EmailAddress { get; set; }
+        [StringLength(100, ErrorMessage = "Cannot Exceed 100 Characters")]
+        public string emailAddress { get; set; }
 
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Confirm Email Address ")]
         [Required(ErrorMessage = "Email Address is Required")]
-        [Compare("EmailAddress",ErrorMessage ="EmailAddresses must match")]
+        [Compare("EmailAddress", ErrorMessage = "EmailAddresses must match")]
         [StringLength(100, ErrorMessage = "Cannot Exceed 100 Characters")]
-        public string ConfirmEmailAddress { get; set; }
+        public string confirmEmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone No")]
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string phoneNo { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = " Date Of Birth")]
+        [Required(ErrorMessage ="Date of birth is required")]
+        public DateTime dateOfBirth { get; set; }
+
+        [Display(Name ="Salary")]
+        [Required(ErrorMessage ="Salary is required")]
+        public long salary { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Password ")]
         [Required(ErrorMessage = "Password  is Required")]
-        public string Password { get; set; }
-
+        public string password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password ")]
         [Required(ErrorMessage = "Confirm Password  is Required")]
-        [Compare("Password", ErrorMessage ="Passwordsmust match")]
-        public string ConfirmPassword { get; set; }
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string confirmPassword { get; set; }
 
+        [Display(Name ="Allot number of leaves")]
+        [Required(ErrorMessage ="number of leaves must be entered" )]
+        public int leavesAvailable { get; set; }
+
+        [Display(Name = "Enter number of credits")]
+        [Required(ErrorMessage = "nuumber of credits must be entered")]
+        public int credits { get; set; }
     }
 }
